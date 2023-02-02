a2b57ecc7b3c2bf1d697de880fac30839a25ac8d
xMaaks <maksimlog222@gmail.com>
2023-02-02 22:17:18 +0300

    Day01: First HTTP server

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+)
 
+func Handler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Test simple HTTP server \n"))
+}
+func main() {
+	http.HandleFunc("/", Handler)
+	log.Println("Start HTTP server on port 777")
+	log.Fatal(http.ListenAndServe(":777", nil))
 }
